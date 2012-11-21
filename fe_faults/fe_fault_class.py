0b5e14ed727f09a16eb75397a55d9cd9ca55f64a
g-weatherill <gaweatherill82@yahoo.co.uk>
2012-11-21 09:12:39 +0100

    Working changes to fe fault classes and strain modeller

--- fe_faults/fe_fault_class.py
+++ fe_faults/fe_fault_class.py
@@ -23,6 +23,15 @@ uncertainty_map = {'Unknown': unc.Uncertainty()
                    }
 
 
+# Parse recurrence_params
+mfd_model_map = {'AndersonLucoType1': AndersonLucoType1(),
+		 'AndersonLucoType2': AndersonLucoType2(),
+		 'AndersonLucoType3': AndersonLucoType3(),
+		 'Characteristic': Characteristic(),
+		 'YoungsCoppersmith': YoungsCoppersmith()}
+
+msr_map = {}
+
 def generate_discrete_normal_probability(mean, sigma, upper, lower, step):
     '''Generates a discrete list of probabilities from a Gaussian distribution
     '''
@@ -139,8 +148,9 @@ class NeotectonicFault(NeotectonicObject):
 class FaultSource(NeotectonicObject):
     '''Fault source object'''
     
-    def __init__(self, identifier, data, compiled_by='Unknown',
-        contributed_by='Unknown', last_updated=datetime.utcnow()):
+    def __init__(self, identifier, data, tectonic_regionalisation, 
+        compiled_by='Unknown', contributed_by='Unknown', 
+        last_updated=datetime.utcnow()):
         ''''''
         super(FaultSource, self).__init__(identifier,
                                           data['name'],
@@ -149,39 +159,167 @@ class FaultSource(NeotectonicObject):
                                           last_updated)
         
         self.tectonic_region = data['tectonic_region']
-        
+        self.msr = []
+        self.number_msr = None
         # Render the geometry
-        self.geometry = self.render_geometry(
-            data['trace'],
-            dip=data['dip'],
-            upper_seismogenic_depth=data['upper_seismogenic_depth'],
-            lower_seismogenic_depth=data['lower_seismogenic_depth'])
+        self.geometry = []
+        for geometry_data in data['Fault_Geometry']: 
+            self.render_geometry(
+                data['trace'],
+                dip=data['dip'],
+                upper_seismogenic_depth=data['upper_seismogenic_depth'],
+                lower_seismogenic_depth=data['lower_seismogenic_depth'])
                                              
 
+    def get_msr(self, input_dict, tectonic_regionalisation):
+        '''Extracts the MSR from the input dictionary'''
+        self.msr = []
+        if not input_dict['MagnitudeScalingRelation']
+            '''Need to use the tectonic regionalisation'''
+            for region in self.tectonic_region:
+                tectonics = tectonic_regionalisation[region['Region_Type']]
+                for msr_value in tectonics['MagnitudeScalingRelation']:
+                    self.msr.append((msr_map[msr_value['Model']], 
+                                     msr_value['Weight']))
+        else:
+            for msr_value in input_dict['MagnitudeScalingRelation']:
+                self.msr.append((msr_map[msr_value['Model']], 
+                                 msr_value['Weight'])
+        self.number_msr = len(self.msr)
+
+    def get_slip(self, input_model):
+        '''Get the slip distribution for the full model'''
+        self.slip_rate = Slip(input_model['slip'], input_model['slip_type'])
+    
+    
+    def get_mfd(self, mfd_input):
+        '''Returns a set of mfd distributions for the source'''
+        number_mfd = len(mfd_input):
+        self.mfd = []
+        for mfd in mfd_input:
+
+            for msr in self.msr:
+                temp_mfd = mfd_model_map[mfd['Model_Type'].setUp(mfd)
+                temp_mfd.get_mmax(
+                    mfd_input, 
+                    msr, 
+                    self.rake, 
+                    self.geometry.area)
+                if 'AndersonLuco' in mfd['ModelType']:
+                self.mfd.append((temp_mfd.get_mfd(}
+
+            if 'AndersonLuco' in temp_mfd:
+                temp_mfd.get_mmax(mfd,  
 
 
+    def _uncertainty_tuple_builder(self):
+        ''''''
+        self.model_tuple = []
+        # Geometry
+        for 
+        
+        
 
+class FaultGeometry(object):
+    '''Class for all geometry definitions'''
+    def __init__(self):
+        '''Instantiate the class
+        '''
+        self.typology = None
+        self.upper_depth = None
+        self.lower_depth = None
+        self.strike = None
+        self.dip = None
+        self.surface = None
+    
 
+    def render_geometry(self, trace, **kwargs):
+        '''
+        Renders the geometry of the section from the trace
+        :param trace: 
+            If a simple fault it is an instance of the nhlib.geo.line.Line
+            class, for a complex fault it is a list of nhlib.geo.line.Line 
+            classes
+        :param **kwargs:
+        '''
+        if (not 'mesh_spacing' in kwargs.keys()) or not kwargs['mesh_spacing']:
+            kwargs['mesh_spacing'] = 1.0
 
+        if isinstance(trace, list):
+            #Complex fault
+            self.typology = 'complex'
+            self.geometry = ComplexFaultSurface.from_fault_data(
+                trace,
+                kwargs['mesh_spacing'])
+            self.dip = self.geometry.get_dip()
+            self.upper_depth = self.geometry.get_top_edge_depth()
+            self.lower_depth = np.max(self.geometry.mesh.depths)
+        else:
+            self.typology = 'simple'
+            self.dip = kwargs['dip']
+            self.upper_depth = kwargs['upper_seismogenic_depth']
+            self.lower_depth = kwargs['lower_seismogenic_depth']
+            self.geometry = SimpleFaultSurface.from_fault_data(
+                trace,
+                self.dip,
+                self.upper_depth,
+                self.lower_depth,
+                kwargs['mesh_spacing'])
+  
 
 class FaultTrace(object):
     '''Class to describe a fault trace'''
     def __init__(self, trace_id, locations,  method='Unknown', name='Unknown', 
                  scale=None, notes=None):
-       '''Instantiate class parsing the geometry to a line trace'''
-       self.identifier = trace_id
-       self.method = method
-       self.name = name
-       self.scale = scale
-       self.notes = notes
-       self.trace = Line([Point(row[0],row[1]) for row in locations])
+        '''Instantiate class parsing the geometry to a line trace'''
+        self.identifier = trace_id
+        self.method = method
+        self.name = name
+        self.scale = scale
+        self.notes = notes
+        self.trace = Line([Point(row[0],row[1]) for row in locations])
 
 
 class Slip(object):
     '''Defines a class of slip parameters'''
+    def __init__(self, slip_dict, slip_type):
+        if not slip_dict['Distribution']:
+            slip_dict['Distribution'] = 'Unknown'
+            self.model = uncertainty_map['Unknown'].create(
+                slip_dict['Preferred'],
+                max=slip_dict['Maximum'],
+                min=slip_dict['Minimum']
+                quality=slip_dict['quality'])
+
+        else:
+            if not 'mean' in slip_dict.keys():
+                slip_dict['mean'] = None
+            
+            if not 'standard_deviation' in slip_dict.keys():
+                slip_dict['standard_deviation'] = None
+
+            self.model = uncertainty_map[slip_dict['Distribution'].create(
+                slip_dict['Preferred'],
+                max=slip_dict['Maximum'],
+                min=slip_dict['Minimum'],
+                quality=slip_dict['completeness'],
+                mean=slip_dict['mean'],
+                standard_deviation=slip_dict['standard_deviation'])
+        
+        self.slip_type = slip_type
+        self.expected = self.model.preferred
+        
+    def get_slip_samples(self, number_samples):
+        ''''''
+        return self.slip.get_samples(number_samples)
 
 
 
+class Displacement(object):
+    '''Defines a class to describe displacement'''
+    def __init__(self, disp_dict):
+        '''
+        '''
 
 #class FaultedEarthFault(object):
 #    '''Master class representation of a Faulted Earth Fault'''
--- strain_modeller/shift_strain_v1.py
+++ strain_modeller/shift_strain_v1.py
@@ -3,25 +3,39 @@
 general model'''
 import abc
 from copy import deepcopy
-from matplotlib.nxutiles import points_inside_poly
-from linecache import getline
+from matplotlib.nxutils import points_inside_poly
+from linecache import getline, getlines
 
 import numpy as np
 
-#T5_column_header =         (/"CRB   ","CTF   ","CCB   ","OSRnor","OSRoth","OTFslo","OTFmed","OTFfas","OCB   ","SUB   "/)
-#T5_CMT_pure_events =       (/   285.9,   198.5,   259.4,   424.3,    77.0,   398.0,   406.9,   376.6,   117.7,  2052.8/)
-#T5_CMT_threshold_moment =  (/ 1.13E17,  3.5E17,  3.5E17, 1.13E17, 1.13E17,  2.0E17,  2.0E17,  2.0E17,  3.5E17,  3.5E17/)
-#T5_beta =                  (/    0.65,    0.65,    0.62,    0.92,    0.82,    0.64,    0.65,    0.73,    0.53,    0.64/)
-#T5_corner_magnitude =      (/    7.64,    8.01,    8.46,    5.86,    7.39,    8.14,    6.55,    6.63,    8.04,    9.58/)
-#T5_tGR_model_moment_rate = (/ 1.67E12,  3.8E12, 1.06E13,  6.7E11,  1.9E11,  6.7E12,  9.4E11,  9.0E11,  4.6E12, 2.85E14/)
-#T5_length_km =             (/  18126.,  19375.,  12516.,  61807.,  61807.,  27220.,  10351.,   6331.,  13236.,  38990./)
-#T5_mean_velocity_mmpa =    (/   18.95,   21.54,   18.16,   46.40,    7.58,   20.68,   57.53,   97.11,   19.22,   61.48/)
-#T5_assumed_dip_degrees =   (/     55.,     73.,     20.,     55.,     55.,     73.,     73.,     73.,     20.,     14./)
-#T5_assumed_mu_GPa =        (/    27.7,    27.7,    27.7,    25.7,    25.7,    25.7,    25.7,    25.7,     49.,     49./)
-#T5_lineIntegral_Nps =      (/   5.5E8,   4.4E8,   6.0E8,   5.0E9,   4.7E8,   5.2E8,   5.3E8,   5.5E8,   1.2E9, 1.58E10/)
-#T5_coupledThickness_km =   (/     3.0,     8.6,     18.,    0.13,    0.40,     13.,     1.8,     1.6,     3.8,     18./)
-#T5_assumed_lithosphere_km =(/      6.,     12.,     13.,      8.,      8.,     14.,     14.,     14.,     14.,     26./)
-#T5_coupling =              (/    0.50,    0.72,    1.00,   0.016,    0.05,    0.93,    0.13,    0.11,    0.27,    0.69/)
+#T5_column_header =        
+# (/"CRB   ","CTF   ","CCB   ","OSRnor","OSRoth","OTFslo","OTFmed","OTFfas","OCB   ","SUB   "/)
+#T5_CMT_pure_events =      
+# (/   285.9,   198.5,   259.4,   424.3,    77.0,   398.0,   406.9,   376.6,   117.7,  2052.8/)
+#T5_CMT_threshold_moment = 
+# (/ 1.13E17,  3.5E17,  3.5E17, 1.13E17, 1.13E17,  2.0E17,  2.0E17,  2.0E17,  3.5E17,  3.5E17/)
+#T5_beta =                 
+# (/    0.65,    0.65,    0.62,    0.92,    0.82,    0.64,    0.65,    0.73,    0.53,    0.64/)
+#T5_corner_magnitude =     
+# (/    7.64,    8.01,    8.46,    5.86,    7.39,    8.14,    6.55,    6.63,    8.04,    9.58/)
+#T5_tGR_model_moment_rate =
+# (/ 1.67E12,  3.8E12, 1.06E13,  6.7E11,  1.9E11,  6.7E12,  9.4E11,  9.0E11,  4.6E12, 2.85E14/)
+#T5_length_km =            
+# (/  18126.,  19375.,  12516.,  61807.,  61807.,  27220.,  10351.,   6331.,  13236.,  38990./)
+#T5_mean_velocity_mmpa =   
+# (/   18.95,   21.54,   18.16,   46.40,    7.58,   20.68,   57.53,   97.11,   19.22,   61.48/)
+#T5_assumed_dip_degrees =   
+# (/     55.,     73.,     20.,     55.,     55.,     73.,     73.,     73.,     20.,     14./)
+#T5_assumed_mu_GPa =       
+# (/    27.7,    27.7,    27.7,    25.7,    25.7,    25.7,    25.7,    25.7,     49.,     49./)
+#T5_lineIntegral_Nps =     
+# (/   5.5E8,   4.4E8,   6.0E8,   5.0E9,   4.7E8,   5.2E8,   5.3E8,   5.5E8,   1.2E9, 1.58E10/)
+#T5_coupledThickness_km =  
+# (/     3.0,     8.6,     18.,    0.13,    0.40,     13.,     1.8,     1.6,     3.8,     18./)
+#T5_assumed_lithosphere_km =
+# (/      6.,     12.,     13.,      8.,      8.,     14.,     14.,     14.,     14.,     26./)
+#T5_coupling =             
+# (/    0.50,    0.72,    1.00,   0.016,    0.05,    0.93,    0.13,    0.11,    0.27,    0.69/)
 
 CRB_PARAMS = {'CMT_EVENTS': 285.9, 'CMT_moment': 1.13E17,
               'beta': 0.65, 'corner_mag': 7.64, 'tGR_moment_rate': 1.67E12,
@@ -69,33 +83,33 @@ OTFmed_PARAMS = {'CMT_EVENTS': 406.9, 'CMT_moment': 2.0E17,
               'beta': 0.65, 'corner_mag': 6.55, 'tGR_moment_rate': 9.4E11,
               'length': 10351., 'velocity': 57.53, 'assumed_dip': 73.,
               'assumed_mu': 25.7, 'line_integral': 5.3E8,
-              'coupled_thickness': 1.8., 'lithosphere': 14.,
+              'coupled_thickness': 1.8, 'lithosphere': 14.,
               'coupling': 0.13, 'adjustment_factor': 1.619}
 
 OTFfas_PARAMS = {'CMT_EVENTS': 376.6, 'CMT_moment': 2.0E17,
               'beta': 0.73, 'corner_mag': 6.63, 'tGR_moment_rate': 9.0E11,
               'length': 6331., 'velocity': 97.11, 'assumed_dip': 73.,
               'assumed_mu': 25.7, 'line_integral': 5.5E8,
-              'coupled_thickness': 1.6., 'lithosphere': 14.,
+              'coupled_thickness': 1.6, 'lithosphere': 14.,
               'coupling': 0.11, 'adjustment_factor': 1.619}
 
 OCB_PARAMS = {'CMT_EVENTS': 117.7, 'CMT_moment': 3.5E17,
               'beta': 0.53, 'corner_mag': 8.04, 'tGR_moment_rate': 4.6E12,
               'length': 13236., 'velocity': 19.22, 'assumed_dip': 20.,
               'assumed_mu': 49., 'line_integral': 1.2E9,
-              'coupled_thickness': 3.8., 'lithosphere': 14.,
+              'coupled_thickness': 3.8, 'lithosphere': 14.,
               'coupling': 0.27, 'adjustment_factor': 2.000}
 
 SUB_PARAMS = {'CMT_EVENTS': 2052.8, 'CMT_moment': 3.5E17,
               'beta': 0.64, 'corner_mag': 9.58, 'tGR_moment_rate': 2.85E14,
-              'length': 38990.., 'velocity': 61.48, 'assumed_dip': 14.,
+              'length': 38990., 'velocity': 61.48, 'assumed_dip': 14.,
               'assumed_mu': 49., 'line_integral': 1.58E10,
               'coupled_thickness': 18., 'lithosphere': 26.,
               'coupling': 0.69, 'adjustment_factor': 3.434}
 
 IPL_PARAMS = {'CMT_EVENTS': 189.0, 'CMT_moment': 3.47E17,
               'beta': 0.63, 'corner_mag': 9.0, 'tGR_moment_rate': None,
-              'length': None., 'velocity': None, 'area': 4.3536E14, 
+              'length': None, 'velocity': None, 'area': 4.3536E14, 
               'assumed_dip': 14., 'assumed_mu': None, 'line_integral': None,
               'coupled_thickness': None, 'lithosphere': None, 'coupling': None, 
               'adjustment_factor': 1.619}
@@ -103,13 +117,14 @@ IPL_PARAMS = {'CMT_EVENTS': 189.0, 'CMT_moment': 3.47E17,
 BIRD_GLOBAL_PARAMETERS = {'CRB': CRB_PARAMS,
                           'CTF': CTF_PARAMS,
                           'CCB': CCB_PARAMS,
-                          'OSRnor': OSRNOR_PARAMS,
-                          'OSRoth': OSROTH_PARAMS,
-                          'OTFslo': OTFSLO_PARAMS,
-                          'OTFmed': OTFMED_PARAMS,
-                          'OTFas': OTFAS_PARAMS,
+                          'OSRnor': OSRnor_PARAMS,
+                          'OSRoth': OSRoth_PARAMS,
+                          'OTFslo': OTFslo_PARAMS,
+                          'OTFmed': OTFmed_PARAMS,
+                          'OTFas': OTFfas_PARAMS,
                           'OCB': OCB_PARAMS,
-                          'SUB': SUB_PARAMS}
+                          'SUB': SUB_PARAMS,
+                          'IPL': IPL_PARAMS}
 
 radian_conv = np.pi / 180.
 secs_per_year = 365.25 * 24. * 60. * 60.
@@ -123,7 +138,8 @@ def moment_function(magnitude):
 
 # Apply SI conversion adjustments from Bird (2007)'s code
 # TODO This is ugly - reconsider this (maybe require only inputs in SI)
-for reg in BIRD_GLOBAL_PARAMETERS:
+for reg_type in BIRD_GLOBAL_PARAMETERS.keys():
+    reg = BIRD_GLOBAL_PARAMETERS[reg_type]
     reg['CMT_pure_event_rate'] = reg['CMT_EVENTS'] / CMT_duration_s
     reg['corner_moment'] = moment_function(reg['corner_mag'])
     if reg != 'IPL':
@@ -139,7 +155,7 @@ for reg in BIRD_GLOBAL_PARAMETERS:
 def tapered_gutenberg_richter_cdf(moment, moment_threshold, beta, 
     corner_moment):
     '''Tapered Gutenberg Richter Cumulative Density Function'''
-    cdf = exp((moment_threshold - moment) / corner_moment)
+    cdf = np.exp((moment_threshold - moment) / corner_moment)
     return ((moment / moment_threshold) ** (-beta)) * cdf
 
 def tapered_gutenberg_richter_pdf(moment, moment_threshold, beta,
@@ -150,9 +166,9 @@ def tapered_gutenberg_richter_pdf(moment, moment_threshold, beta,
         corner_moment)
 
 # Regional data classes
-class RegionalParameters(object):
-    '''Abstract base class'''
-    __metaclass__ = abc.ABCMeta
+#class RegionalParameters(object):
+#    '''Abstract base class'''
+#    __metaclass__ = abc.ABCMeta
 
 
 class Strain(object):
@@ -191,7 +207,8 @@ class Strain(object):
         self.number_points = np.shape(input_data)[0]
         for iloc, name in enumerate(header_names):
             self.strain_data[name] = input_data[:, iloc]
-
+        #print self.strain_data
+        #breaker = here
         invalid_long = self.strain_data['long'] > 180.
         if np.any(invalid_long):
             # Strain Data is Pacific-centred
@@ -202,49 +219,47 @@ class Strain(object):
             print 'Deriving Kreemer Regionalisation'
             # Pre-allocate region list
             self.strain_data['region'] = \
-                [None for xval in range(0, self.number_points)]
+                [ for xval in range(0, self.number_points)]
 
-            west_limit = np.min(self.strian_data['long'])
-            east_limit = np.max(self.strian_data['long'])
-            north_limit = np.max(self.strian_data['lat'])
-            south_limit = np.min(self.strian_data['lat'])
+            west_limit = np.min(self.strain_data['long'])
+            east_limit = np.max(self.strain_data['long'])
+            north_limit = np.max(self.strain_data['lat'])
+            south_limit = np.min(self.strain_data['lat'])
             regionalisation = self.apply_kreemer_regionalisation(north_limit,
                 south_limit, east_limit, west_limit)
-            for polygon in kreemer_polygons:
-                id0 = np.where(np.logical_and(
+            for polygon in regionalisation:
+                idlong = np.logical_and(
                     self.strain_data['long'] >= polygon['long_lims'][0],
-                    self.strain_data['long'] < polygon['long_lims'][1],
+                    self.strain_data['long'] < polygon['long_lims'][1])
+                id0 = np.logical_and(idlong, 
                     self.strain_data['lat'] >= polygon['lat_lims'][0],
-                    self.strain_data['lat'] < polygon['lat_lims'][1])[0])
+                    self.strain_data['lat'] < polygon['lat_lims'][1])
                 if len(np.where(id0)[0]) > 0:
                     for iloc in id0:
                         self.strain_data['region'][iloc] = \
                             polygon['region_type']
-         else:
-             # If 'region' was in the header then the data dictionary will
-             # contain a list of NaNs 
-             datafile = open(input_strain_file, 'rt')
-             for iloc, row in enumerate(csv.DictReader(datafile)):
-                 self.strain_data['region'][iloc] = row['region']
-         self.strain_data['region'] = np.array(self.strain_data['region'])
-
-
-        
-
-        
+        else:
+            # If 'region' was in the header then the data dictionary will
+            # contain a list of NaNs 
+            datafile = open(input_strain_file, 'rt')
+            for iloc, row in enumerate(csv.DictReader(datafile)):
+                self.strain_data['region'][iloc] = row['region']
+        self.strain_data['region'] = np.array(self.strain_data['region'])
 
 
     def apply_kreemer_regionalisation(self, north=90., south = -90., 
-        east = -180., west = 180.):
+        east = 180., west = -180.):
         '''Applies the regionalisation of Kreemer (2003)'''
-        input_data = getlines('tectonic_areas.dat.txt')
+        input_data = getlines('data/tectonic_areas.dat.txt')
         kreemer_polygons = []
         temp_dict = {'cell': None, 'region_type':'', 'long_lims':None,
                      'lat_lims': None}
+        
         for line_loc, line in enumerate(input_data):
             if '>' in line[0]:
                 polygon_dict = deepcopy(temp_dict)
                 polygon_dict['region_type'] = line[2].rstrip('\n')
+                #print input_data, line_loc
                 polygon_dict['cell'] = self._build_kreemer_cell(input_data, 
                                                                 line_loc)
                 polygon_dict['long_lims'] = np.array([
@@ -254,7 +269,8 @@ class Strain(object):
                     np.min(polygon_dict['cell'][:, 1]),
                     np.max(polygon_dict['cell'][:, 1])])
                 polygon_dict['cell'] = None
-                if polygon_dict['long_lims'][1] > 180
+                #print polygon_dict
+                if polygon_dict['long_lims'][1] > 180:
                     polygon_dict['long_lims'] = \
                         polygon_dict['long_lims'] - 360.0
                 if (polygon_dict['long_lims'][0] >= west) and\
@@ -267,16 +283,17 @@ class Strain(object):
         # Apply point in polygon
         
     def _build_kreemer_cell(self, data, loc):
-        temp_poly = np.array([4,2], dtype = float)
-        for ival in range(0,4):
+        temp_poly = np.empty([4,2], dtype = float)
+        for ival in range(1,5):
             value = data[loc + ival].rstrip('\n')
             value = value.lstrip(' ')
             value = np.array((value.split(' ',1))).astype(float)
-            temp_poly[ival,:] = value
+            #print ival, value
+            temp_poly[ival - 1,:] = value.flatten()
         return temp_poly
 
 
-    def shift(self, minimum_magnitude) 
+    def shift(self, minimum_magnitude): 
             #regionalisation = BIRD_GLOBAL_PARAMETERS):
         '''Applies SHIFT model - python implementation of the 
         SHIFT_continuum_seismicity subroutine in SHIFT_GSRM.f90'''
@@ -310,11 +327,13 @@ class Strain(object):
         returns a vector of the corresponding seismicity rates'''
         
         strain_values = np.column_stack([e1h, e2h, err])
-        e1_rate = np.min(strain_data, dimension=1)
+        e1_rate = np.min(strain_data, dimension = 1)
         e3_rate = np.max(strain_data, dimension = 1)
         e2_rate = 0. - e1_rate - e3_rate
         # Pre-allocate seismicity rate with zeros
-        seismicity_rate = np.zeros(np.shape(strain_values)[0], dtype=float)
+        seismicity_rate = np.zeros(
+            [np.shape(strain_values)[0], len(threshold_moment)], 
+            dtype=float)
         # Calculate moment rate per unit area
         temp_e_rate = 2.0 * (-e1_rate)
         id0 = np.where(e2_rate < 0.0)[0]
@@ -328,15 +347,12 @@ class Strain(object):
             (M_persec_per_m2 / region_params['tGR_moment_rate'])
         # Adjust forecast rate to desired rate using tapered G-R model
         # Taken from Eq 7 (Bird et al. 2010) and Eq 9 (Bird & Kagan, 2004)
-        arg_check = (region_params['CMT_moment'] - threshold_moment) /\
-            region_params['corner_moment']
-        id0 = arg_check >= -100.0
-        
-        seismicity_rate[id0] = ((threshold_moment / 
-            region_params['CMT_moment']) ** region_pars['beta']) * \
-            np.exp(arg_check[id0]) * seismicity_at_cmt_threshold * \
-            region_params['adjustment_factor']
+        for iloc, moment_thresh in enumerate(threshold_moment):
+            arg_check = (region_params['CMT_moment'] - moment_thresh) /\
+		         region_params['corner_moment']
+            id0 = arg_check >= -100.0
+            seismicity_rate[id0, iloc] = ((moment_thresh / 
+                region_params['CMT_moment']) ** region_pars['beta']) * \
+                np.exp(arg_check[id0]) * seismicity_at_cmt_threshold * \
+                region_params['adjustment_factor']
         return seismicity_rate
-        
-
-
